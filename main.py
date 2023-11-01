6933f541954fdb52026171f2b8f262a531ad45ca
ldrobnik <drobniklukasz@gmail.com>
2023-11-01 19:53:50 +0100

    Define Transcribe a Sequence query

--- main.py
+++ main.py
@@ -8,4 +8,11 @@ app = FastAPI()
 def complement_sequence(sequence: str):
     initial_sequence = Seq(sequence)
     complementary_sequence = initial_sequence.complement()
-    return { str(complementary_sequence)}
+    return { str(complementary_sequence) }
+
+@app.get("/transcribe/{sequence}")
+def transcribe_sequence(sequence: str):
+    initial_sequence = Seq(sequence)
+    transcribed_sequence = initial_sequence.transcribe()
+    return { str(transcribed_sequence) }
+
