4992af7d8c6b7b0b94534215e2d791fd94e9aa23
ldrobnik <drobniklukasz@gmail.com>
2023-11-01 19:41:30 +0100

    Define Complement a Sequence query

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/complement/{sequence}")
+def complement_sequence(sequence: str):
+    initial_sequence = Seq(sequence)
+    complementary_sequence = initial_sequence.complement()
+    return { str(complementary_sequence)}
